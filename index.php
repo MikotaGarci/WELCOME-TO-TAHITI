d7f8920e6af61852f8c144492aab152853845126
Скляр Євген <145337848+MikotaGarci@users.noreply.github.com>
2024-12-08 17:55:12 +0000

    3

--- index.php
+++ index.php
@@ -6,18 +6,23 @@
 <section class="cards-section">
     <div class="cards">
         <div class="card">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/tahiti1.jpg" alt="Bora Bora">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/tahiti1.jpg" alt="BORA BORA">
             <h3>BORA BORA</h3>
             <p>From $2,500</p>
         </div>
         <div class="card">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/tahiti2.jpg" alt="Moorea">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/tahiti2.jpg" alt="MOOREA">
             <h3>MOOREA</h3>
             <p>From $2,500</p>
         </div>
         <div class="card">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/tahiti3.jpg" alt="Raiatea">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/tahiti3.jpg" alt="RAISTEA">
             <h3>RAI'TEA</h3>
+            <p>From $2,500</p>
+        </div>
+        <div class="card">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/tahiti2.jpg" alt="TAHA'A">
+            <h3>MOOREA</h3>
             <p>From $2,500</p>
         </div>
     </div>
