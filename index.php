7dd48c97f42dc2d923d14cd79a64462810647613
Скляр Євген <145337848+MikotaGarci@users.noreply.github.com>
2024-12-08 17:42:04 +0000

    3

--- index.php
+++ index.php
@@ -6,17 +6,17 @@
 <section class="cards-section">
     <div class="cards">
         <div class="card">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/tahiti.jpg" alt="Bora Bora">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/tahiti1.jpg" alt="Bora Bora">
             <h3>BORA BORA</h3>
             <p>From $2,500</p>
         </div>
         <div class="card">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/tahiti.jpg" alt="Moorea">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/tahiti2.jpg" alt="Moorea">
             <h3>MOOREA</h3>
             <p>From $2,500</p>
         </div>
         <div class="card">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/tahiti.jpg" alt="Raiatea">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/tahiti3.jpg" alt="Raiatea">
             <h3>RAI'TEA</h3>
             <p>From $2,500</p>
         </div>
